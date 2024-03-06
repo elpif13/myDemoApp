cfdd412c4823da0576919b3c62dc19113f9b61b8
elpif13 <elifbayar1313@gmail.com>
2024-03-06 21:07:05 +0300

    First implementation of App.java is done.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,17 +7,55 @@ import java.util.ArrayList;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-    public static boolean search(ArrayList<Integer> array, int e) {
-        System.out.println("inside search");
-        if (array == null) return false;
+    /*It takes the given index range of the arrays, 
+    converts the integer values ​​in this range into characters, 
+    and checks whether the two strings are anagrams.*/
+    
+    public static boolean search(ArrayList<Integer> array,ArrayList<Integer> array2, int start, int end) {
+        
+        //-------edge cases------------
+        if(array == null || array2 == null)
+          return false;
 
-        for (int elt : array) {
-            if (elt == e) return true;
+        if(start>end || start<0)
+          return false;
+
+        int length = start - end + 1; // length of index
+        if(array.size()<length || array2.size()<length)
+          return false;
+
+        if(end >= array.size() || end >= array2.size())  
+          return false;
+
+        //---------------------------
+
+        String str1 = "",str2=""; // default
+
+        // convert arrays to string
+        for(int i=start; i<=end; i++){
+            if(array.get(i)>=32 && array.get(i)<=126)
+            str1 += (char)array[i];
+            
+            if(array2.get(i)>=32 && array2.get(i)<=126)
+            str2 += (char)array2[i];
         }
-        return false;
+
+        int[] charCount1 = new int[256];
+        int[] charCount2 = new int[256];
+
+        for (int i = 0; i < str1.length(); i++) {
+            charCount1[str1.charAt(i)]++;
+            charCount2[str2.charAt(i)]++;
+        }
+
+        // check the freakans of chars
+        for (int i = 32; i <= 126; i++) {
+            if (charCount1[i] != charCount2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+
     }
 }
