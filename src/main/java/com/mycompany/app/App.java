bb7e8ccf8d23fce9fd96c8aa48692ab1d3615420
elpif13 <elifbayar1313@gmail.com>
2024-03-08 11:58:59 +0300

    Some bugs are fixed.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,44 +5,46 @@ import java.util.ArrayList;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    /*It takes the given index range of the arrays, 
-    converts the integer values ​​in this range into characters, 
+    /*It takes the given index range of the arrays,
+    converts the integer values ​​in this range into characters,
     concatinates the two strings and returns.*/
-    
-    public static String search(ArrayList<Integer> array,ArrayList<Integer> array2, int start, int end) {
-        
+
+    public static String search(int[] array,int[] array2, int start, int end) {
+
         //-------edge cases------------
         if(array == null || array2 == null)
-          return false;
+            return null;
 
         if(start>end || start<0)
-          return false;
+            return null;
 
         int length = start - end + 1; // length of index
-        if(array.size()<length || array2.size()<length)
-          return false;
+        if(array.length<length || array2.length<length)
+            return null;
 
-        if(end >= array.size() || end >= array2.size())  
-          return false;
+        if(end >= array.length || end >= array2.length)
+            return null;
 
         //---------------------------
 
-        String str1 = "",str2=""; // default
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+
 
         // convert arrays to string
         for(int i=start; i<=end; i++){
-            if(array.get(i)>=32 && array.get(i)<=126)
-            str1 += (char)array[i];
-            
-            if(array2.get(i)>=32 && array2.get(i)<=126)
-            str2 += (char)array2[i];
+            if(array[i]>=32 && array[i]<=126)
+                str1.append((char) array[i]);
+
+            if(array2[i]>=32 && array2[i]<=126)
+                str2.append((char) array2[i]);
         }
 
-        
-        return str1+str2;
-        
+
+        return str1.toString()+str2.toString();
+
 
     }
 }
